3f13d431a50393badcd83612b6e750d2eda7222d
ReAlist967 <bolkisev.ilya@mail.ru>
2024-05-18 20:58:22 +0500

    modified text recognition

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import cv2
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
 
-from recognition_model import get_model
+from recognition_model import get_model, ALPHABET
 
 
 def get_cols_and_rows(img):
@@ -49,17 +48,17 @@ def process_image(img, shape_to=(64, 256)):
     ---
     img : np.array
     """
-    w, h, _ = img.shape
+    w, h = img.shape
     new_w = shape_to[0]
     new_h = int(h * (new_w / w))
     img = cv2.resize(img, (new_h, new_w))
-    w, h, _ = img.shape
+    w, h = img.shape
 
     img = img.astype('float32')
 
     new_h = shape_to[1]
     if h < new_h:
-        add_zeros = np.full((w, new_h - h, 3), 255)
+        add_zeros = np.full((w, new_h - h), 255)
         img = np.concatenate((img, add_zeros), axis=1)
 
     if h > new_h:
@@ -68,21 +67,73 @@ def process_image(img, shape_to=(64, 256)):
     return img
 
 
+# TRANSLATE INDICIES TO TEXT
+def indicies_to_text(indexes, idx2char):
+    text = "".join([idx2char[i] for i in indexes])
+    text = text.replace('EOS', '').replace('PAD', '').replace('SOS', '')
+    return text
+
+
+def prediction(model, imgs, idx2char, device):
+    """
+    params
+    ---
+    model : nn.Module
+    test_dir : str
+        path to directory with images
+    id2char : dict
+        map from indicies to chars
+
+    returns
+    ---
+    preds : dict
+        key : name of image in directory
+        value : dict with keys ['p_value', 'predicted_label']
+    """
+    preds = []
+    p_imgs = []
+    with torch.no_grad():
+        for img in imgs:
+            if min(img.shape) == 0:
+                preds.append('')
+                p_imgs.append(None)
+                continue
+
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.GaussianBlur(img, (3, 3), 0)
+            img = 255 - cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+            img = process_image(img).astype('uint8')
+            img = np.expand_dims(img, -1)
+            img = img / img.max()
+            p_imgs.append(img)
+            img = np.transpose(img, (2, 0, 1))
+
+            src = torch.FloatTensor(img).unsqueeze(0).to(device)
+            out_indexes = model.predict(src)
+            pred = indicies_to_text(out_indexes[0], idx2char)
+            preds.append(pred)
+
+    return preds, p_imgs
+
+
 img = cv2.imread('../../Downloads/table_sample.jpg')[:, :, ::-1]
 
 device = 'cpu'
-model = get_model(device)
+model = get_model().to(device)
 model.load_state_dict(torch.load('weights/recognition_model.pt'))
-model.eval()
 
 cols, rows, v_shift, h_shift = get_cols_and_rows(img)
-for i in range(1, rows.shape[0]):
-    # detect row number
-    segment = img[rows[i-1]-v_shift:rows[i]+v_shift, :cols[0]]
-    segment = process_image(segment)
 
-    with torch.no_grad():
-        pred = model.predict(torch.tensor(segment).unsqueeze(0).to(torch.float32))
-
-    plt.imshow(segment)
-    plt.show()
+ids = []
+segments = [img[rows[i-1]+v_shift:rows[i], :cols[0]] for i in range(1, rows.shape[0])]
+idx2char = {idx: char for idx, char in enumerate(ALPHABET)}
+pred, processed = prediction(model, segments,  idx2char, device)
+print(processed[0].shape)
+for i in range(len(pred)):
+    if processed[i] is None:
+        print(i, 'Skipped')
+        continue
+    print(i, pred[i])
+    cv2.imshow(str(i), processed[i])
+    cv2.waitKey()
