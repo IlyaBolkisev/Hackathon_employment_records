29485e4d068eb9726946f644043fcd4e83bec3a6
ReAlist967 <bolkisev.ilya@mail.ru>
2024-05-19 08:37:39 +0500

    minor fix

--- modules/wrapper.py
+++ modules/wrapper.py
@@ -19,11 +19,13 @@ def wrapper(imgs):
     recognition_model.load_state_dict(torch.load('weights/recognition_model.pt'))
 
     for img in imgs:
+        layout.append([])
         tables, coords = detect_document(localization_model, img)
         for table, (x_t, y_t) in zip(tables, coords):
-            layout.append([])
+            layout[-1].append([])
             cols, rows, v_shift, h_shift = get_cols_and_rows(table)
             if cols is None:
+                del layout[-1][-1]
                 continue
             rows_height = np.median([rows[i] - rows[i-1] for i in range(1, len(rows))]).astype(int)
             rows = validate_rows(rows, rows_height)
@@ -33,14 +35,14 @@ def wrapper(imgs):
 
             stamps = detect_stamps(table)
             for i in range(len(block)):
-                layout[-1].append(generate_empty_dict())
+                layout[-1][-1].append(generate_empty_dict())
                 for i, category in enumerate(['id', 'date', 'info', 'doc']):
                     if i + 1 < len(cols):
-                        layout[-1][-1][category] = {"text": "", "x": x_t+cols[i], "y": y_t+block[i][0],
+                        layout[-1][-1][-1][category] = {"text": "", "x": x_t+cols[i], "y": y_t+block[i][0],
                                                     "w": cols[i-1], "h": block[i][1] - block[i][0]}
 
                 for x, y, r in stamps:
                     if block[i][0] < y <= block[i][1]:
-                        layout[-1][-1]['stamp'] = {"text": "", "x": int(x_t+x-r), "y": int(y_t+y-r),
+                        layout[-1][-1][-1]['stamp'] = {"text": "", "x": int(x_t+x-r), "y": int(y_t+y-r),
                                                    "w": int(2*r), "h": int(2*r)}
     return layout
